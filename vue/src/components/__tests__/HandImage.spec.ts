9973165c0a97af0deb9cdec16ec5dbbce9362dcf
Daniel Methfessel <daniel.methfessel@gmail.com>
2024-01-12 10:25:42 +0100

    CI failure.

--- vue/src/components/__tests__/HandImage.spec.ts
+++ vue/src/components/__tests__/HandImage.spec.ts
@@ -7,8 +7,20 @@ import { ref } from 'vue'
 
 describe('HandImage', () => {
   it('renders properly', () => {
-    const wrapper = mount(HandImage, { props: { hand: ref(Hand.Rock) } })
+    const wrapper = mount(HandImage, { props: { hand: ref(Hand.Rock), isPlayer: false } })
     expect( wrapper.element.tagName.toLowerCase()).toEqual('img')
     expect(wrapper.element.getAttribute('alt')?.toLocaleLowerCase()).toContain('rock')
+  })
+
+  it('provides the right tooltip for result', () => {
+    const wrapper = mount(HandImage, { props: { hand: ref(Hand.Paper), isPlayer: true } })
+    expect( wrapper.element.tagName.toLowerCase()).toEqual('img')
+    expect(wrapper.element.getAttribute('title')?.toLocaleLowerCase()).toContain("paper")
+  })
+
+  it('provides the right tooltip for clickable', () => {
+    const wrapper = mount(HandImage, { props: { hand: ref(Hand.Paper), isPlayer: false } })
+    expect( wrapper.element.tagName.toLowerCase()).toEqual('img')
+    expect(wrapper.element.getAttribute('title')?.toLocaleLowerCase()).toContain("paper (press '2')")
   })
 })
